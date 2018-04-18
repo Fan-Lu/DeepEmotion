c6c29d18617c4e8bc0e414e19a7b81995e0e01b2
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-04-18 18:04:06 -0400

    Add files via upload

---
+++ capsuleNet_TF/CNN-capsule_fer2013.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 15:45:18 2018
+
+@author: yuxi
+"""
+
+"""Train a simple CNN-Capsule Network on thefer2013 face emotion dataset.
+Without Data Augmentation:
+This is a fast Implement, just 20s/epcoh with a gtx 1070 gpu.
+"""
+
+
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras import activations
+from keras import utils
+from keras.datasets import cifar10
+from keras.models import Model
+from keras.layers import *
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D,AveragePooling2D,Input,Lambda,Reshape
+import tensorflow as tf
+import numpy as np
+import keras
+from keras import callbacks
+
+
+# the squashing function.
+# we use 0.5 in stead of 1 in hinton's paper.
+# if 1, the norm of vector will be zoomed out.
+# if 0.5, the norm will be zoomed in while original norm is less than 0.5
+# and be zoomed out while original norm is greater than 0.5.
+def squash(x, axis=-1):
+    s_squared_norm = K.sum(K.square(x), axis, keepdims=True) + K.epsilon()
+    scale = K.sqrt(s_squared_norm) / (0.5 + s_squared_norm)
+    return scale * x
+
+
+# define our own softmax function instead of K.softmax
+# because K.softmax can not specify axis.
+def softmax(x, axis=-1):
+    ex = K.exp(x - K.max(x, axis=axis, keepdims=True))
+    return ex / K.sum(ex, axis=axis, keepdims=True)
+
+
+# define the margin loss like hinge loss
+def margin_loss(y_true, y_pred):
+    lamb, margin = 0.5, 0.1
+    return K.sum(y_true * K.square(K.relu(1 - margin - y_pred)) + lamb * (
+        1 - y_true) * K.square(K.relu(y_pred - margin)), axis=-1)
+
+
+class Capsule(Layer):
+    """A Capsule Implement with Pure Keras
+    There are two vesions of Capsule.
+    One is like dense layer (for the fixed-shape input),
+    and the other is like timedistributed dense (for various length input).
+    The input shape of Capsule must be (batch_size,
+                                        input_num_capsule,
+                                        input_dim_capsule
+                                       )
+    and the output shape is (batch_size,
+                             num_capsule,
+                             dim_capsule
+                            )
+    Capsule Implement is from https://github.com/bojone/Capsule/
+    Capsule Paper: https://arxiv.org/abs/1710.09829
+    """
+
+    def __init__(self,
+                 num_capsule,
+                 dim_capsule,
+                 routings=3,
+                 share_weights=True,
+                 activation='squash',
+                 **kwargs):
+        super(Capsule, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.share_weights = share_weights
+        if activation == 'squash':
+            self.activation = squash
+        else:
+            self.activation = activations.get(activation)
+
+    def build(self, input_shape):
+        input_dim_capsule = input_shape[-1]
+        if self.share_weights:
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(1, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+        else:
+            input_num_capsule = input_shape[-2]
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(input_num_capsule, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='glorot_uniform',
+                trainable=True)
+
+    def call(self, inputs):
+        """Following the routing algorithm from Hinton's paper,
+        but replace b = b + <u,v> with b = <u,v>.
+        This change can improve the feature representation of Capsule.
+        However, you can replace
+            b = K.batch_dot(outputs, hat_inputs, [2, 3])
+        with
+            b += K.batch_dot(outputs, hat_inputs, [2, 3])
+        to realize a standard routing.
+        """
+
+        if self.share_weights:
+            hat_inputs = K.conv1d(inputs, self.kernel)
+        else:
+            hat_inputs = K.local_conv1d(inputs, self.kernel, [1], [1])
+
+        batch_size = K.shape(inputs)[0]
+        input_num_capsule = K.shape(inputs)[1]
+        hat_inputs = K.reshape(hat_inputs,
+                               (batch_size, input_num_capsule,
+                                self.num_capsule, self.dim_capsule))
+        hat_inputs = K.permute_dimensions(hat_inputs, (0, 2, 1, 3))
+
+        b = K.zeros_like(hat_inputs[:, :, :, 0])
+        for i in range(self.routings):
+            c = softmax(b, 1)
+            
+            if K.backend() == 'theano':
+                o = K.sum(o, axis=1)
+            o = self.activation(K.batch_dot(c, hat_inputs, [2, 2]))
+            
+            if i < self.routings - 1:
+                b = K.batch_dot(o, hat_inputs, [2, 3])
+                if K.backend() == 'theano':
+                    o = K.sum(o, axis=1)
+
+        return o
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_capsule, self.dim_capsule)
+
+
+batch_size = 128
+num_classes = 7
+epochs = 15
+#%%load data
+def loadfer2013():
+    
+    tmp = np.loadtxt("fer2013.csv", dtype=np.str, delimiter=",")
+    reader = tmp[1:,1]
+    TRAIN_END_POINT = len(reader)
+    train_data_x = np.zeros([TRAIN_END_POINT, 48, 48], dtype="uint8")
+    for k, data in enumerate(reader):
+       pixels_formated = [int(a) for a in data.split(" ")] 
+       pixels_in_picture_format = np.reshape(pixels_formated, [48, 48])
+       train_data_x[k, :, :] = pixels_in_picture_format
+    label = tmp[1:,0].astype(np.uint8)
+    return train_data_x, label
+
+data, label = loadfer2013()
+#%%
+x_train = data[0:28700]
+y_train = label[0:28700]
+x_validation = data[28709:32298]
+y_validation = label[28709:32298]
+x_test = data[32298:]
+y_test = label[32298:]
+#%%
+x_train = x_train.reshape(-1, 48, 48, 1).astype('float32') / 255.
+x_validation = x_validation.reshape(-1, 48, 48, 1).astype('float32') / 255.
+x_test = x_test.reshape(-1, 48, 48, 1).astype('float32') / 255.
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_validation = keras.utils.to_categorical(y_validation, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+#%%
+# A common Conv2D model
+input_image = Input(shape=(None, None, 1))
+x = Conv2D(64, (3, 3), activation='relu')(input_image)
+x = Conv2D(64, (3, 3), activation='relu')(x)
+x = AveragePooling2D((2, 2))(x)
+x = Conv2D(128, (3, 3), activation='relu')(x)
+x = Conv2D(128, (3, 3), activation='relu')(x)
+
+
+"""now we reshape it as (batch_size, input_num_capsule, input_dim_capsule)
+then connect a Capsule layer.
+the output of final model is the lengths of 7 Capsule, whose dim=16.
+the length of Capsule is the proba,
+so the problem becomes a 7 two-classification problem.
+"""
+
+x = Reshape((-1, 128))(x)
+capsule = Capsule(7, 16, 3, True)(x)
+output = Lambda(lambda z: K.sqrt(K.sum(K.square(z), 2)))(capsule)
+model = Model(inputs=input_image, outputs=output)
+model.summary()
+
+# we use a margin loss
+log = callbacks.CSVLogger('log.csv')
+checkpoint = callbacks.ModelCheckpoint('CapsuleNet_weights-{epoch:02d}.h5',save_best_only=True, save_weights_only=True, verbose=1)
+#model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+model.compile(loss=margin_loss, optimizer='adam', metrics=['accuracy']) 
+
+model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1, validation_data=(x_validation, y_validation),callbacks=[log,checkpoint])
+score = model.evaluate(x_test, y_test, verbose=0)
+
+model.save_weights('trained_model_capsuleNet.h5')
+print('Trained model saved to \'trained_model.h5\'')
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# we can compare the performance with or without data augmentation
+#data_augmentation = False
+data_augmentation = False
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(
+        x_train,
+        y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(x_test, y_test),
+        shuffle=True)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by dataset std
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in 0 to 180 degrees
+        width_shift_range=0.1,  # randomly shift images horizontally
+        height_shift_range=0.1,  # randomly shift images vertically
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    # Compute quantities required for feature-wise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(
+        datagen.flow(x_train, y_train, batch_size=batch_size),
+        epochs=epochs,
+        validation_data=(x_test, y_test),
+        workers=4)
