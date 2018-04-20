6ec739bc2891b3a02c8a189f7910890aa786ab3e
FAN LU <fanlu207@gmail.com>
2018-04-20 14:29:45 -0400

    update via upload

--- src/main.py
+++ src/main.py
@@ -12,12 +12,12 @@ import argparse
 parser = argparse.ArgumentParser(description='CapsNet')
 parser.add_argument('--batchsize', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=100, metavar='N',
+parser.add_argument('--epochs', type=int, default=250, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--cuda', action='store_true', default=True,
-                    help='disables CUDA training')
+                    help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
@@ -29,8 +29,7 @@ parser.add_argument('--with_reconstruction', action='store_true', default=False)
 args = parser.parse_args()
 
 
-
-def train(epoch):
+def get_train():
     datacsv = GetDataFromCSV()
     image, labels = datacsv.get_training_data()
     labels = torch.from_numpy(labels)
@@ -41,9 +40,32 @@ def train(epoch):
     f_l = f_l / 255.0
     f_l = torch.from_numpy(f_l)
     train_dataset = MyDataset(f_l, labels)
-
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batchsize, shuffle=True, num_workers=2)
+    return train_loader
+
+
+def get_test():
+    datacsv = GetDataFromCSV()
+    image, labels = datacsv.get_test_data()
+    labels = torch.from_numpy(labels)
+    f_l = np.empty([7178, 1, 48, 48])
+    for index, item in enumerate(f_l):  # Refill the list
+        item[0] = image[index]
+    f_l = f_l.astype("float32")
+    f_l = f_l / 255.0
+    f_l = torch.from_numpy(f_l)
+    test_dataset = MyDataset(f_l, labels)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batchsize, shuffle=False, num_workers=2)
+    return test_loader
+
+
+train_loader = get_train()
+test_loader = get_train()
+
+
+def train(epoch):
     model.train()
+    correct = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         if args.cuda:
             data, target = data.cuda(), target.cuda()
@@ -51,13 +73,38 @@ def train(epoch):
         data, target = Variable(data), Variable(target, requires_grad=False)
         optimizer.zero_grad()
         output, probs = model(data)
+        pred = probs.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        acc = 100.*correct/((batch_idx+1)*args.batchsize)
         loss = loss_fn(probs, target)
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.8f}\t Acc: {:.0f}%\t'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.data[0]))
+                       100. * batch_idx / len(train_loader), loss.data[0], acc))
+
+
+def test():
+    model.load_state_dict(torch.load('080_model_dict_3routing_reconstructionFalse.pth'))
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for i, (data, target) in enumerate(test_loader):
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+            target = target.type(torch.cuda.LongTensor)
+        data, target = Variable(data, volatile=True), Variable(target)git
+        output, probs = model(data)
+        test_loss += loss_fn(probs, target.view(-1), size_average=False).data[0]
+        pred = probs.data.max(1, keepdim=True)[1]   #get the index of the max probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        print(i)
+    test_loss /=len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return test_loss, correct
 
 
 if __name__ == "__main__":
@@ -73,6 +120,8 @@ if __name__ == "__main__":
 
     for epoch in range(1, args.epochs + 1):
         train(epoch)
-        torch.save(model.state_dict(),
-                   '{:03d}_model_dict_{}routing_reconstruction{}.pth'.format(epoch, args.routing_iterations,
-                                                                             args.with_reconstruction))
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(),
+                       '{:03d}_model_dict_{}routing_reconstruction{}.pth'.format(epoch, args.routing_iterations,
+                                                                                 args.with_reconstruction))
+    # test_loss, accuracy = test()
